7da85958bd5666417377530cdf4c67d5c63d7112
Lissa Squeens <78526444+nyakeisi@users.noreply.github.com>
2023-01-02 20:33:18 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,3 +3,5 @@ export * from './lib/table';
 export * from './lib/token';
 export * from './lib/snowflake';
 
+export * from './lib/adapters/LocalStorage';
+export * from './lib/adapters/ExternalConnection';
